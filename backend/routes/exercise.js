eaf3d892b3bfeaffc9c9a9eef295fd36bc6b9350
steven cain <stevencainn@gmail.com>
2020-01-20 00:02:25 -0500

    finished delete and update routes for exercises

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -27,4 +27,34 @@ router.route('/add').post((req, res) =>{
     .catch(err => res.status(400).json('Error:  ' + err));
 });
 
+// get request to return the exercise based on the ID 
+router.route('/:id').get((req, res) =>{
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('error:  ', err));
+});
+
+//delete request to delete exercise based on ID
+router.route('/:id').delete((req, res) =>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("exercise deleted"))
+    .catch(err => res.status(400).json('error:  ', err));
+});
+
+router.route('/update/:id').post((req, res) =>{
+    Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(() => res.json("exercise updated!"))
+        .catch(err => res.status(400).json('error:  ', err))
+    })
+    .catch(err => res.status(400).json('error:  ', err));
+});
+
+
 module.exports = router;
