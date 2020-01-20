b0cccca01b30c64646441aea2a3f8a4488a27dfb
steven cain <stevencainn@gmail.com>
2020-01-19 21:01:46 -0500

    set up mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express"); 
 const cors = require("cors");
+const mongoose = require("mongoose");
+const mongojs = require("mongojs")
 
 require("dotenv").config();
 
@@ -8,7 +9,25 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+var databaseUrl = "";
+var collections = [""];
+
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+  });
+  
+  db.on("connect", function() {
+    console.log("Database Connected:");
+});
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`)
