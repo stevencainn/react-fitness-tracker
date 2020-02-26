661ce5f9653558b8aaa4bc4e204604c4320d9e9c
steven cain <stevencainn@gmail.com>
2020-02-25 23:27:48 -0500

    successfully made front end ui

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,8 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
+const Exercise = props =>(
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <a href="#" onClick={() => {props.deleteExercise(props.exercise._id)}}>delete</a>
+    </td>
+  </tr>
+)
 
 
 export default class ExercisesList extends Component {
+  constructor(props){
+    super(props);
+    this.deleteExercise = this.deleteExercise.bind(this);
+    this.state = {
+      exercises: []
+    };
+  }
+
+  componentDidMount(){
+    Axios.get('http://localhost:5000/exercises/')
+    .then(response =>{
+      this.setState({
+        exercises: response.data
+      })
+    })
+    .catch((error) =>{
+      console.log(error)
+    })
+  }
+
+  deleteExercise(id){
+    Axios.delete('http://localhost:5000/exercises/'+id)
+    .then(res => console.log(res.data));
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
+
+  exerciseList(){
+    return this.state.exercises.map(currentexercise =>{
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+    })
+  }
+
   render() {
     return (
       <div>
@@ -17,6 +64,9 @@ export default class ExercisesList extends Component {
               <th>Actions</th>
             </tr>
           </thead>
+          <tbody>
+            {this.exerciseList()}
+          </tbody>
         </table>
       </div>
     )
