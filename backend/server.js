96f5a34e700cb45115350a70dfd98d45607e54ef
steven cain <stevencainn@gmail.com>
2020-03-19 19:58:14 -0400

    will the / fix it all?

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   const path = require('path');
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 };
