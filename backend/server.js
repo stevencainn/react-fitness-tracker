e6800eb70a84c3ae85869a1cc6449e6f9e10678e
steven cain <stevencainn@gmail.com>
2020-03-19 18:57:33 -0400

    heroku stuff

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 // mongoose.connect("mongodb://localhost:27017/fitness-tracker", { useNewUrlParser: true });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/fitness-tracker",
