1e22d16378b3934282b0630896c8917ddeea5b3e
steven cain <stevencainn@gmail.com>
2020-06-18 11:06:48 -0400

    seperated db connection from server file

--- models/index.js
+++ models/index.js
@@ -8,9 +8,14 @@ mongoose.connect(process.env.MONGODB_URI || URI,
      useNewUrlParser: true,
      useUnifiedTopology: true
     }
-   );
-   
-   const connection = mongoose.connection;
-   connection.once('open', () => {
-    console.log('Connected to mLab MongoDB Database successfully');
-   })
+);
+
+//when succesfully connected
+mongoose.connection.on('connected', () => {
+    console.log('Established Mongoose Default Connection');
+});
+
+//when connection error 
+mongoose.connection.on('error', err => {
+    console.log('Mongoose Default Connection Error : ' + err);
+});
