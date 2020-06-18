c764014bfc2f2c469a56625bf8e7b1a488ebe7eb
steven cain <stevencainn@gmail.com>
2020-06-18 13:43:08 -0400

    heroku test

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -13,7 +13,7 @@ router.route('/add').post((req, res) =>{
 
     newUser.save()
     .then(() => res.json('User Added!!'))
-    .catch(err => res.status(400).json(err))
+    .catch(err => res.status(400).json('Error:', err));
 });
 
 //delete request to delete exercise based on ID
