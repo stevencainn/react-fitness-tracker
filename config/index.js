3419fb475f61c984e7208bfb7cd07d9bd841dc70
steven cain <stevencainn@gmail.com>
2020-06-18 10:54:03 -0400

    set up config file

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const dbUser = `fitness`;
+const dbPassword = `fitness1`;
+
+const MONGODB_URI = `mongodb://${dbUser}:${dbPassword}@ds247637.mlab.com:47637/heroku_8bff610v`;
+
+module.exports = MONGODB_URI;
+
