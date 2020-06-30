f687295e7f8add2efc02150ce34b4ef456ab5415
steven cain <stevencainn@gmail.com>
2020-06-30 16:46:47 -0400

    controllers and route work

--- controllers/exercises_controller.js
+++ controllers/exercises_controller.js
@@ -8,13 +8,43 @@ exports.getExercises = async (req, res, next) => {
 
   return res.status(200).json({
    success: true,
-   count: exercises.length,
    data: exercises
   });
  } catch (err) {
   return res.status(500).json({
    success: false,
-   error: '.:.:SERVER ERROR:.:.'
+   error: '.:.:SERVER ERROR GETTING EXERCISE:.:.'
   });
  }
 }
+
+
+exports.addExercises = async (req, res, next) => {
+    try {
+     const { username, description, duration, date } = req.body
+
+     const exercises = await Exercise.create(req.body);
+   
+     return res.status(200).json({
+      success: true,
+      data: exercises
+     });
+    } catch (err) {
+     return res.status(500).json({
+      success: false,
+      error: '.:.:SERVER ERROR ADDING EXERCISE:.:.'
+     });
+    }
+}
+
+exports.addExercises = async (req, res, next) => {
+    try {
+    
+    } catch (err) {
+     return res.status(500).json({
+      success: false,
+      error: '.:.:SERVER ERROR:.:.'
+     });
+    }
+}
+
--- routes/exercisesRouter.js
+++ routes/exercisesRouter.js
@@ -2,26 +2,37 @@
 
 const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
+const { getExercises } = require("../controllers/exercises_controller");
+
+router.route('/')
+    .get(getExercises);
+
+router.route('/add')
+    .post(addExercises);
+
+
+
+
 
 //get request that returns all exercises
-router.route('/').get((req, res) => {
-    Exercise.find()
-        .then(exercises => res.json(exercises))
-        .catch(err => res.status(400));
-});
+// router.route('/').get((req, res) => {
+//     Exercise.find()
+//         .then(exercises => res.json(exercises))
+//         .catch(err => res.status(400));
+// });
 
 // add new exercise to db
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const description = req.body.description;
-    const duration = Number(req.body.duration);
-    const date = Date.parse(req.body.date);
-    const newExercise = new Exercise({
-        username,
-        description,
-        duration,
-        date,
-    });
+// router.route('/add').post((req, res) => {
+//     const username = req.body.username;
+//     const description = req.body.description;
+//     const duration = Number(req.body.duration);
+//     const date = Date.parse(req.body.date);
+//     const newExercise = new Exercise({
+//         username,
+//         description,
+//         duration,
+//         date,
+//     });
     newExercise.save()
         .then(() => res.json('Exercise added'))
         .catch(err => res.status(400));
